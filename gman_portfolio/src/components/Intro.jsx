c65f1f1598536d262c7d9825edf9852a5d59787c
Garrett Mandrona <gwmandrona@gmail.com>
2024-02-01 19:14:08 -0600

    Working on adding text to the main page, was running into some issues with the `className` addition in the js not actually changing the text.

--- gman_portfolio/src/components/Intro.jsx
+++ gman_portfolio/src/components/Intro.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 function Intro() {
     return (
-        <div className="flex items-center
-            justify-center flex-col text-center pt-20
-            pb-6">Intro</div>
+        <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+            <h1 className = "text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Garrett</h1>
+            <p className = "text-base md:text-xl mb-3 font-medium">Security Engineer & Technology Management Student</p>
+            <p>This is where the bio will go</p>
+        </div>
         // Go to tailwindcss.com/dpcs/padding to see the documentation on what padding to use
     )
 }
